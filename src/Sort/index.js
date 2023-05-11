6e1df148bcd5add5dd458c3e9e5cb69aae9483c8
CesarQuint <pedrocesarquintero001@gmail.com>
2023-05-10 22:32:16 -0600

    Selection Sort

--- src/Sort/index.js
+++ src/Sort/index.js
@@ -2,7 +2,7 @@
 //* Cada elemento se compará así con su inmediato sucesor para ordenarse según sea mayor o menor que este. Una vez la pareja está ordenada correctamente, se toma el último número de cada tupla para volver a compararlo con el siguiente.
 
 const bubbleSort = arreglo => {
-    const longitud = arreglo.length;
+    const longitud = arreglo.length
     for (let i = 0; i < longitud; i++ ) {
       for (let j = 0; j < longitud - 1 - i; j++ ) {
         let actual = arreglo[ j ]
@@ -14,10 +14,48 @@ const bubbleSort = arreglo => {
       }
     }
   
-    return arreglo;
-  };
+    return arreglo
+  }
 
+//!!Insertion Sort
+//TODO:BUSCAR MAS INFORMACION ACERCA DE LOS ALGORITMOS
+//*En lugar de comparar a > b y hacer esto en bucle este alritmo compara a > b ? a > c : arreglo[a] = arreglo [b] ..... en resumidas cuentas este algoritmo compara un item con toda la cadena hasta encontrar su lugar 
+
+const insertionSort = arreglo => {
+    const longitud = arreglo.length;
+    let posicionActual
+    let temp
+  
+    for ( let i = 1; i < longitud; i++ ) {
+      posicionActual = i;
+      temp = arreglo[ i ];
+      while ( posicionActual > 0 && arreglo[ posicionActual - 1 ] > temp ) {
+        arreglo[ posicionActual ] = arreglo[ posicionActual - 1 ];
+        posicionActual--;
+      }
+      arreglo[ posicionActual ] = temp;
+    }
+  
+    return arreglo;
+  }
+  
+//!!Selection Sort 
+//*Algoritmo que busca el menor de los valores y lo mueve al comienzo este se mantiene recursivo hasta mover los valores la diferencia con el anterior es que compara a < [...arreglo]
+
+const selectionSort = arreglo => {
+    for ( let j = 0; j < arreglo.length; ++j ) {
+      let i = iMin = j;
+      for ( ++i; i < arreglo.length; ++i ) {
+        ( arreglo[ i ] < arreglo[ iMin ] ) && ( iMin = i );
+      }
+      [ arreglo[ j ], arreglo[ iMin ] ] = [ arreglo[ iMin ], arreglo[ j ] ];
+    }
+  
+    return arreglo;
+  }
 
 export{
-    bubbleSort
+    bubbleSort,
+    insertionSort,
+    selectionSort
 }
--- src/components/Algorithm.jsx
+++ src/components/Algorithm.jsx
@@ -1,11 +1,13 @@
 import { useEffect,useState } from 'react'
-import {bubbleSort} from '../Sort'
+import {bubbleSort,insertionSort} from '../Sort'
 
 function Algorithm() {
 
  
   console.log( bubbleSort([0,10,2,8,1]))
-  
+
+  console.log("-".repeat(10));
+  console.log(insertionSort([2,0,10,14,15,22]));
   return (
     <>
     <p>Creacion y testeo de algoritmos</p>
