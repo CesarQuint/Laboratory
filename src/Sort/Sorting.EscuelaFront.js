357f7d94a5ef1ef112ac11bb70b21414fca41bd9
CesarQuint <pedrocesarquintero001@gmail.com>
2023-05-24 19:38:05 -0600

    Learning Sorting Algorithm

--- src/Sort/Sorting.EscuelaFront.js
+++ src/Sort/Sorting.EscuelaFront.js
@@ -48,4 +48,41 @@ const bubbleSortRecursivity = arr => {
 
 //!!Selection Sort
 
-console.log(bubbleSortRecursivity(arrExamp));
+//-Recorre todo el arreglo hasta seleccionar el elemento que cumple con los criterios impuestos
+
+const SelectionSort = arr => {
+ 
+    for (let i = 0; i < arr.length; ++i) {
+        let j = min = i
+        for (++j ; j < arr.length; ++j) {
+            ( arr[ j ] < arr[ min ] ) && ( min = j );
+        }
+        [ arr[ i ], arr[ min ] ] = [ arr[ min ], arr[ i ] ]
+    }
+    return arr
+}
+
+//!Complejidad O(n^2)
+
+//!Insertion Sort 
+
+const insertionSort = arreglo => {
+    const longitud = arreglo.length;
+    let posicionActual
+    let temp
+  
+    for ( let i = 1; i < longitud; i++ ) {
+      posicionActual = i;
+      temp = arreglo[ i ];
+      while ( posicionActual > 0 && arreglo[ posicionActual - 1 ] > temp ) {
+        arreglo[ posicionActual ] = arreglo[ posicionActual - 1 ];
+        posicionActual--;
+      }
+      arreglo[ posicionActual ] = temp;
+    }
+  
+    return arreglo;
+  }
+  
+
+
