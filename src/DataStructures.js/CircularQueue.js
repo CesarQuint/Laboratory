5fde57ed4ebb418e0a55b56a32c491e4c65c4e4d
CesarQuint <pedrocesarquintero001@gmail.com>
2023-07-18 23:11:38 -0600

    Circular Queue Implementation

--- src/DataStructures.js/CircularQueue.js
+++ src/DataStructures.js/CircularQueue.js
@@ -16,12 +16,49 @@ class CircularQueue {
 
 	enqueue(element) {
 		if (!this.isFull()) {
-			this.rear = this.rear + 1
+			this.rear = (this.rear + 1) % this.capacity
 			this.items[this.rear] = element
 			this.currentLength += 1
 			if (this.front === -1) {
 				this.front = this.rear
 			}
+		}
+	}
+
+	dequeue() {
+		if (this.isEmpty()) {
+			return null
+		}
+		const item = this.items[this.front]
+		this.items[this.front] = null
+		this.front = (this.front + 1) % this.capacity
+		this.currentLength -= 1
+		if (this.isEmpty()) {
+			this.front = -1
+			this.rear = -1
+		}
+		return item
+	}
+
+	peek() {
+		if (!this.isEmpty()) {
+			return this.items[this.front]
+		}
+		return null
+	}
+
+	print() {
+		if (this.isEmpty()) {
+			console.log('Queue is Empty')
+		} else {
+			let i
+			let str = ''
+			for (i = this.front; i !== this.rear; i = (i + 1) % this.capacity) {
+				str += this.items[i] + ''
+			}
+
+			str += this.items[i]
+			console.log(str)
 		}
 	}
 }
