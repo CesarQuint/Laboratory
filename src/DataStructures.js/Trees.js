71be647ecdd485fcd15659ce17fc1b1dc3d30f84
CesarQuint <pedrocesarquintero001@gmail.com>
2023-08-23 23:41:18 -0600

    Binary Search

--- src/DataStructures.js/Trees.js
+++ src/DataStructures.js/Trees.js
@@ -2,3 +2,10 @@
 //-A tree is a non-linear data structure ,compared to arrays ,linked list ,stacks and queue which are linear data structures
 //!Trees however ,owing to the nonlinear nature allow quicker and easier access to the data
 //! A TREE WILL NOT CONTAIN ANY LOOP OR CYCLE
+
+//- Scenarios ( File System for directory structure , A family tree , An organisation tree , DOM)
+
+//* Tree Terminology
+//*The firs node is root
+//- The nodes that doesn't have any child are leaves
+//-Degree is the quantity of edges a node possess
