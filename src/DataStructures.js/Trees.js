7922c3f68ce259e922cb43da46a91ee0202eb7b2
CesarQuint <pedrocesarquintero001@gmail.com>
2023-08-29 23:27:21 -0600

    Binary Tree & Graph

--- src/DataStructures.js/Trees.js
+++ src/DataStructures.js/Trees.js
@@ -99,7 +99,72 @@ class BinarySearchTree {
 		}
 	}
 
-	postOrder(root) {}
+	postOrder(root) {
+		if (root) {
+			this.postOrder(root.left)
+			this.postOrder(root.right)
+			console.log(root.value)
+		}
+	}
+
+	levelOrder() {
+		//Use the optimized queue
+		const queue = []
+		queue.push(this.root)
+		while (queue.length) {
+			let curr = queue.shift()
+			console.log(curr.value)
+			if (curr.left) {
+				queue.push(curr.left)
+			}
+			if (curr.right) {
+				queue.push(curr.right)
+			}
+		}
+	}
+
+	min(root) {
+		if (!root.left) {
+			return root.value
+		} else {
+			return this.min(root.left)
+		}
+	}
+
+	max(root) {
+		if (!root.right) {
+			return root.value
+		} else {
+			return this.max(root.right)
+		}
+	}
+
+	delete(value) {
+		this.root = this.deleteNode(this.root, value)
+	}
+
+	deleteNode(root, value) {
+		if (root === null) {
+			return root
+		}
+		if (value < root.value) {
+			root.left = this.deleteNode(root.left, value)
+		} else if (value > root.value) {
+			root.right = this.deleteNode(root.right, value)
+		} else {
+			if (!root.left && !root.right) {
+				return null
+			}
+			if (!root.left) {
+				return root.right
+			} else if (!root.right) {
+				return root.left
+			}
+			root.value = this.min(root.right)
+			root.right = this.deleteNode(root.right, root.value)
+		}
+		return root
+	}
 }
 
 const bst = new BinarySearchTree()
@@ -117,4 +182,4 @@ console.log(bst.search(bst.root, 5))
 console.log(bst.search(bst.root, 15))
 console.log(bst.search(bst.root, 25))
 
-bst.inOrder(bst.root)
+bst.levelOrder()
