0a2ece4a1d717b44eaf7f50abace165ec447d05b
CesarQuint <pedrocesarquintero001@gmail.com>
2023-10-10 14:46:59 -0600

    Array Bid

--- src/LeetCode2.js/LeetCode7.js
+++ src/LeetCode2.js/LeetCode7.js
@@ -59,4 +59,20 @@ function minimumTwice(arr) {
   return arr.indexOf(max);
 }
 
-console.log(minimumTwice(vars));
+const test1 = [2, 9, 6, 9, 9];
+
+const sum1 = digits => {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (digits[i] < 9) {
+      digits[i]++;
+      return digits;
+    } else {
+      digits[i] = 0;
+    }
+  }
+
+  digits.unshift(1);
+  return digits;
+};
+
+console.log(sum1(test1));
