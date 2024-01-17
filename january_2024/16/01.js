f73b09220a84d782dd435d265a5b7a13106dc900
CesarQuint <pedrocesarquintero001@gmail.com>
2024-01-16 20:24:09 -0600

    FindWinner , maxFrequency , uniqueOccurrences

--- january_2024/16/01.js
+++ january_2024/16/01.js
@@ -24,3 +24,72 @@ var largestPerimeter = function (nums) {
   // Reject: impossible to make triangle
   return 0;
 };
+
+var findWinners = function (matches) {
+  let losses = new Array(100001).fill(0);
+
+  for (const [winner, loser] of matches) {
+    if (losses[winner] === 0) {
+      losses[winner] = -1;
+    }
+
+    if (losses[loser] === -1) {
+      losses[loser] = 1;
+    } else {
+      losses[loser]++;
+    }
+  }
+
+  let zeroLoss = [];
+  let oneLoss = [];
+
+  for (let i = 1; i <= 100000; ++i) {
+    if (losses[i] === -1) {
+      zeroLoss.push(i);
+    } else if (losses[i] === 1) {
+      oneLoss.push(i);
+    }
+  }
+
+  return [zeroLoss, oneLoss];
+};
+
+var maxFrequencyElements = function (nums) {
+  let map = new Map();
+  for (let num of nums) {
+    map.set(num, (map.get(num) || 0) + 1);
+  }
+  let max = 0;
+  for (let frequency of map.values()) {
+    max = Math.max(max, frequency);
+  }
+  let res = 0;
+  for (let frequency of map.values()) {
+    if (frequency === max) {
+      res += frequency;
+    }
+  }
+  return res;
+};
+
+var uniqueOccurrences = function (arr) {
+  let myMap = new Map();
+
+  for (let num of arr) {
+    if (myMap.has(num)) {
+      myMap.set(num, myMap.get(num) + 1);
+    } else {
+      myMap.set(num, 1);
+    }
+  }
+
+  let mySet = new Set();
+
+  for (const val of myMap.values()) {
+    if (mySet.has(val)) return false;
+
+    mySet.add(val);
+  }
+
+  return true;
+};
