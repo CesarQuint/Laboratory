5866083705ac49408e56185eb9402ef43be41d11
CesarQuint <pedrocesarquintero001@gmail.com>
2024-01-17 21:07:14 -0600

    Multiply & MyPow

--- january_2024/17/01.js
+++ january_2024/17/01.js
@@ -14,3 +14,41 @@ var addBinary = function (a, b) {
   const sum = BigInt(aBin) + BigInt(bBin);
   return sum.toString(2);
 };
+
+var multiply = function (num1, num2) {
+  if (num1 === '0' || num2 === '0') return '0';
+
+  const m = num1.length,
+    n = num2.length,
+    res = new Array(m + n).fill(0);
+
+  for (let i = m - 1; i >= 0; i--) {
+    for (let j = n - 1; j >= 0; j--) {
+      const p1 = i + j,
+        p2 = i + j + 1;
+      let sum = res[p2] + Number(num1[i]) * Number(num2[j]);
+      res[p2] = sum % 10;
+      res[p1] += Math.floor(sum / 10);
+    }
+  }
+  if (res[0] === 0) res.shift();
+  return res.join('');
+};
+
+var myPow = function (x, n) {
+  if (n === 0) {
+    return 1;
+  }
+
+  if (n < 0) {
+    x = 1 / x;
+    n = -n;
+  }
+
+  if (n % 2 === 0) {
+    let halfPower = myPow(x, n / 2);
+    return halfPower * halfPower;
+  } else {
+    return x * myPow(x, n - 1);
+  }
+};
